21338f1191a82bb32f082487069873652941ff84
ciarannolan <ciaran.nolan.3@ucdconnect.ie>
2019-09-28 19:13:27 +0100

    .

--- src/ie/ucd/game/BoardReader.java
+++ src/ie/ucd/game/BoardReader.java
@@ -6,6 +6,7 @@
 
 package ie.ucd.game;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,16 +19,43 @@ import java.util.StringTokenizer;
 
 public class BoardReader {
 
+	private ArrayList<String> propertyNames = new ArrayList<String>(22);  
+
     public void readProperties() throws IOException {
 
-    	BufferedReader input = new BufferedReader(new FileReader("ie/ucd/gameConfigurations/property.properties"));
+    	/*BufferedReader input = new BufferedReader(new FileReader("src/ie/ucd/gameConfigurations/property.properties"));
     	String line;
     	while((line = input.readLine()) != null){
-			System.out.println(line);		
-		}
+			
+    		
+    		
+    		System.out.println(line);		
+		}*/
     	
- 
-      
+    	try {
+    		Properties prop = new Properties();
+    		
+    		String propFileName = "ie/ucd/gameConfigurations/property.properties";
+    		InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+    		
+    		if (inputStream != null) {
+				prop.load(inputStream);
+			} else {
+				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+			}
+    		
+    	for(int i=0; i<=22; i++) {
+    		propertyNames.add(prop.getProperty("squareName0")); 
+    	}
+    		
+    	} catch (Exception e) {
+			System.out.println("Exception: " + e);
+    	}
+    }
+    
+    public ArrayList<String> getPropertyNames(){
+    	return this.propertyNames;
+    	
+    }
     }
 
-}
--- src/ie/ucd/game/readerTest.java
+++ src/ie/ucd/game/readerTest.java
@@ -1,9 +1,13 @@
 package ie.ucd.game;
+import java.io.IOException;
+
 import ie.ucd.game.BoardReader;
 
 public class readerTest {
-	public static void main(String[] args) { 
-		
+	public static void main(String[] args) throws IOException { 
+	BoardReader b1 = new BoardReader();
+	b1.readProperties();
+	System.out.println(b1.getPropertyNames());
 	}
 
 }
