f88ff4c427d77bef590bc3c2910602587c4f7f99
Ciaran Nolan <cnnolan2@gmail.com>
2019-12-01 22:50:17 +0000

    Code Clean

---
+++ test/ie/ucd/game/CanOwnTest.java
@@ -0,0 +1,24 @@
+package ie.ucd.game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CanOwnTest {
+
+    @Test
+    void getTitleDeedCard() {
+    }
+
+    @Test
+    void setTitleDeedCard() {
+    }
+
+    @Test
+    void mortgage() {
+    }
+
+    @Test
+    void demortgage() {
+    }
+}
