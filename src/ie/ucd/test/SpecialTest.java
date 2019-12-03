6579803ee2e8185672361ea50374b1d8702780d4
Robert Keenan <robertmkeenan1@gmail.com>
2019-12-03 19:51:33 +0000

    Special test added. More functionality to TitleDeed tests

---
+++ src/ie/ucd/test/SpecialTest.java
@@ -0,0 +1,49 @@
+package ie.ucd.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SpecialTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testSpecial() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testImplementSpecialSquare() {
+		fail("Not yet implemented");
+	}
+
+}
